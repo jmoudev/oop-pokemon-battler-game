f09cd6078c5dc26067a12f22aa9f0caa65595c3d
JJYoko101 <mould_joe@hotmail.com>
2021-02-24 18:50:22 +0000

    creates pokemon class with basic attributes

---
+++ pokemon-battler.test.js
@@ -0,0 +1,22 @@
+const { Pokemon } = require('./pokemon-battler');
+
+let bulbasaur;
+
+beforeEach(() => {
+  bulbasaur = new Pokemon('bulbasaur', 50, 60, 'arrghh');
+});
+
+describe('Pokemon class', () => {
+  it('return pokemon with a name property given as argument', () => {
+    expect(bulbasaur.name).toBe('bulbasaur');
+  });
+  it('return pokemon with a hitPoints property given as argument', () => {
+    expect(bulbasaur.hitPoints).toBe(50);
+  });
+  it('return pokemon with an attackDamage property given as argument', () => {
+    expect(bulbasaur.attackDamage).toBe(60);
+  });
+  it('return pokemon with a sound property given as argument', () => {
+    expect(bulbasaur.cry).toBe('arrghh');
+  });
+});
