fa8a3c8245aef20603023ae1e5336d29525246d6
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-24 18:50:22 +0000

    creates pokemon class with basic attributes

---
+++ pokemon-battler.js
@@ -0,0 +1,10 @@
+class Pokemon {
+  constructor(name, hitPoints, attackDamage, cry) {
+    this.name = name;
+    this.hitPoints = hitPoints;
+    this.attackDamage = attackDamage;
+    this.cry = cry;
+  }
+}
+
+module.exports = { Pokemon };
