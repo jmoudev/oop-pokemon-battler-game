ec7dce728e541872b0c26bb36b2f8efa3262b027
JJYoko101 <mould_joe@hotmail.com>
2021-03-07 12:38:36 +0000

    creates first two questions and script

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var inquirer = require('inquirer');
 inquirer
-  .prompt([])
-  .then(answers => {
-    // Use user feedback for... whatever!!
+  .prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: "What's your first name?"
+    },
+    {
+      type: 'list',
+      name: 'starter_pokemon',
+      message: 'Please choose your starter Pokemon',
+      choices: ['Bulbasaur', 'Charmander', 'Squirtle', 'Pikachu']
+    }
+  ])
+  .then(({ first_name, starter_pokemon }) => {
+    console.log(`Hey ${first_name}....`);
+    setTimeout(() => {
+      console.log(`Your starter Pokemon is ${starter_pokemon}!`);
+    }, 1000);
   })
   .catch(error => {
     if (error.isTtyError) {
