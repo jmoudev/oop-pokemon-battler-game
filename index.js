319b8d266c71a495f2aee84365567327b66e85f9
JJYoko101 <mould_joe@hotmail.com>
2021-03-07 14:09:53 +0000

    refactors function to async await with timer function

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'first_name',
-      message: "What's your first name?"
-    },
-    {
-      type: 'list',
-      name: 'starter_pokemon',
-      message: 'Please choose your starter Pokemon',
-      choices: ['Bulbasaur', 'Charmander', 'Squirtle', 'Pikachu']
-    }
-  ])
-  .then(({ first_name, starter_pokemon }) => {
+
+const CLIStarter = async () => {
+  const timeout = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+
+  try {
+    const { first_name, starter_pokemon } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: "What's your first name?"
+      },
+      {
+        type: 'list',
+        name: 'starter_pokemon',
+        message: 'Please choose your starter Pokemon',
+        choices: ['Bulbasaur', 'Charmander', 'Squirtle', 'Pikachu']
+      }
+    ]);
+
     console.log(`Hey ${first_name}....`);
-    setTimeout(() => {
-      console.log(`Your starter Pokemon is ${starter_pokemon}!`);
-    }, 1000);
-  })
-  .catch(error => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+
+    await timeout(1000);
+
+    console.log(`Your starter Pokemon is ${starter_pokemon}!`);
+  } catch (err) {
+    console.log(err.isTtyError);
+  }
+};
+
+const CLIBattle = async () => {};
+
+const pokemonGame = async () => {
+  await CLIStarter();
+  await CLIBattle();
+};
